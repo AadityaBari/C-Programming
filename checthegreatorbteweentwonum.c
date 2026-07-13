508683fe3bb49205d6f90d4b2a29036e53ff4da3
Aaditya Prasad <aadityaprasad2025@gmail.com>
2026-07-13 13:26:52 +0530

    Add files via upload

---
+++ checthegreatorbteweentwonum.c
@@ -0,0 +1,18 @@
+//write  a program to check weather to greater number
+#include<stdio.h>
+void main ()
+{
+	int a ,b;
+	printf ("Enter the value of a and b");
+	scanf("%d%d",&a,&b);
+	if (a>b)
+	{
+		printf("A is greator than B");
+		
+	}
+	else
+	{
+		printf ("B is greator than A");
+	}
+
+}
---
+++ divisibleby5and11.c
@@ -0,0 +1,23 @@
+//write aprogram TO CHECK WHEATHER a number is divided by 5 and 11
+#include<stdio.h>
+void main ()
+{
+
+int num;
+printf("Enter the num");
+scanf("%d",&num);
+if(num%5==0 && num%11==0)
+{
+	printf("divided by5 and 11");
+}
+else
+{
+	printf(" not divided by 5 and 11");
+	
+}
+
+
+
+
+
+}
