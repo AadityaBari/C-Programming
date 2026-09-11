88b8046a470a893fd527a1564a643d9b1b5919d1
Aaditya Prasad <aadityaprasad2025@gmail.com>
2026-09-11 23:00:24 +0530

    Add files via upload

---
+++ assingmentno.3.c
@@ -0,0 +1,11 @@
+//write aprogranm to calculate area of rectangle
+#include<stdio.h>
+void main ()
+{
+	float l ,b,area ;
+	printf("Enter the value of lenght and bright");
+	scanf("%f%f",&l,&b);
+	area = l*b;
+	printf("area of rectangle =%f",area);
+	
+}
