cf069f8b22a4fe8ead4a41038c10c5b34ef46d32
Aaditya Prasad <aadityaprasad2025@gmail.com>
2026-09-12 19:38:20 +0530

    Add files via upload

---
+++ simpleinterst.c
@@ -0,0 +1,11 @@
+// write aprogram to simplwe interest
+#include<stdio.h>
+ int main ()
+{
+	int p,r,t,si;
+	printf(" Enter the value of p,r,t");
+	scanf("%d d% %d",&p,&r,&t);
+	si =p*r*t/100;
+	printf(" value of simple interest =%d",si);
+	
+}
